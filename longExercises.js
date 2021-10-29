6650288f61a20f8da4d3cc0e37da8b87eb6165b4
Mahed <mahedyar@gmail.com>
2021-10-29 19:53:22 +0330

    Subasa exercise added

--- longExercises.js
+++ longExercises.js
@@ -51,3 +51,61 @@ const password = (k , string) => {
 
 ////////////////////////////////// Subasa Exercise //////////////
 
+// function sortChecker (prev , current) {
+//     // if (current - prev > 0){
+//     //     return true
+//     // }
+//     // if (current - prev <= 0){
+//     //     return false
+//     // }
+// }
+function sortChecker (array) {
+    let arrayMax = 0
+
+    for (let i =0 ; i<array.length ; i++ ){
+        if (array[i] < arrayMax) {
+            return false
+        }
+        arrayMax = array[i]
+    }
+    return true
+}
+
+
+
+function subasa (n , firstMinutes, secondMinutes , goals) {
+
+    let firstHalf = []
+    let secondHalf = []
+    let counter = 0
+    let max = 0
+
+    for (let i=0  ; i<n ; i++) {
+        if (goals[i] <= 45+firstMinutes && goals[i] > max ) {
+            firstHalf.push(goals[i])
+            counter++
+        }
+        max = goals[i]
+    }
+
+    for (let j= counter ; j<n ; j++){
+        if( 45 <= goals[j] && goals[j] <= 90+secondMinutes ){
+            secondHalf.push(goals[j])
+        }
+    }
+
+    if (sortChecker(firstHalf) && sortChecker(secondHalf) && goals[n-1] <= 90+secondMinutes) {
+        console.log("YES")
+    } else {
+        console.log("NO")
+    }
+
+
+    // console.log(sortChecker(firstHalf))
+    // console.log(sortChecker(secondHalf))
+    // console.log(firstHalf)
+    // console.log(secondHalf)
+
+}
+
+subasa(6, 3, 2, [8,48,45,70 , 81 , 94 ])
